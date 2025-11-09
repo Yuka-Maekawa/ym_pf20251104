6decd89bf5201927b41441278aafc58f1fb8628c
Yuka-Maekawa <yamayuri_0806@yahoo.co.jp>
2025-11-09 22:19:52 +0900

    【ガチャ】ボタンを押したらガチャのリザルトシーンへ移動する処理を追加

---
+++ Assets/Script/MyProject/Scene/Gacha/GachaMenu/GachaMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace MyProject.Gacha.Menu
+{
+    public class GachaMenuController : MonoBehaviour
+    {
+        [SerializeField] private GachaMenuUIController _uiController = null;
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void Initialize()
+        {
+            _uiController.Initialize();
+        }
+
+        /// <summary>
+        /// 解放
+        /// </summary>
+        public void Release()
+        {
+            _uiController.Release();
+        }
+    }
+}
---
+++ Assets/Script/System/ResourceManager/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace MyProject.Systems.Resource
+{
+    public partial class SceneLoader : SingletonMonoBehaviour<SceneLoader>
+    {
+        private Loader _sceneLoad = null;
+        public static Loader SceneLoad => Instance._sceneLoad;
+
+        protected override async UniTask Initialize()
+        {
+            await UniTask.CompletedTask;
+            _sceneLoad = new Loader();
+        }
+
+        /// <summary>
+        /// 解放
+        /// </summary>
+        protected override void Release()
+        {
+            _sceneLoad = null;
+        }
+    }
+}
