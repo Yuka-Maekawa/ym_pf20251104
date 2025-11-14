5f702e2533d6344ece7f96ad447d0a91c95c4666
Yuka-Maekawa <yamayuri_0806@yahoo.co.jp>
2025-11-14 16:54:51 +0900

    【ランチャー】開始シーンを追加

---
+++ Assets/Script/MyProject/Scene/Launcher/LauncherMain.cs
@@ -0,0 +1,25 @@
+﻿using Cysharp.Threading.Tasks;
+using MyProject.Systems.Resource;
+using UnityEngine;
+
+namespace MyProject.Launcher
+{
+    public class LauncherMain : MonoBehaviour
+    {
+        private static readonly string _backScenePath = "Scene/Gacha/GachaMenu";
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void Initialize()
+        {
+            SceneLoader.SceneLoad.LoadSceneAsync(_backScenePath).Forget();
+        }
+
+        /// <summary>
+        /// 解放
+        /// </summary>
+        public void Release() { }
+    }
+}
+
