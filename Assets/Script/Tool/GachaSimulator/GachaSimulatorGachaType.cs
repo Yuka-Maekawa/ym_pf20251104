0254f74e4872629f6c621a12e9fc3440decfca93
Yuka-Maekawa <yamayuri_0806@yahoo.co.jp>
2025-11-14 23:47:07 +0900

    【ガチャシミュレーター】アセットとシーン準備

---
+++ Assets/Script/Tool/GachaSimulator/GachaSimulatorGachaType.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace MyProject.Tool.GachaSimulator
+{
+    public class GachaSimulatorGachaType : MonoBehaviour
+    {
+        /// <summary>
+        ///  初期化
+        /// </summary>
+        public void Initialize()
+        {
+
+        }
+
+        /// <summary>
+        /// 解放
+        /// </summary>
+        public void Release()
+        {
+
+        }
+    }
+}
---
+++ Assets/Script/Tool/GachaSimulator/GachaSimulatorMain.cs
@@ -0,0 +1,43 @@
+﻿using Cysharp.Threading.Tasks;
+using MyProject.Common.UI;
+using UnityEngine;
+
+namespace MyProject.Tool.GachaSimulator
+{
+    public class GachaSimulatorMain : MonoBehaviour
+    {
+        private enum State
+        {
+            Idle,
+            Home,
+            InputPlayCount,
+            InputGachaType,
+            Simulation,
+            WriteFile
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void Initialize()
+        {
+            InitializeAsync().Forget();
+        }
+
+        /// <summary>
+        /// 初期化(非同期)
+        /// </summary>
+        public async UniTask InitializeAsync()
+        {
+            await FadeManager.Fade.PlayFadeInAsync();
+        }
+
+        /// <summary>
+        /// 解放
+        /// </summary>
+        public void Release()
+        { 
+}
+
+    }
+}
